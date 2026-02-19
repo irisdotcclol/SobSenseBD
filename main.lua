631c1c72496485295ea7efce135ea2c4da84a9a4
RustyDev <29099@palmerton.org>
2026-02-19 12:18:06 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,32 @@
+repeat task.wait() until game:IsLoaded()
+if shared.vape then shared.vape:Uninject() end
+
+if identifyexecutor then
+    if table.find({'Argon', 'Wave'}, ({identifyexecutor()})[1]) then
+        getgenv().setthreadidentity = nil
+    end
+end
+
+local vape
+local loadstring = function(...)
+    local res, err = loadstring(...)
+    if err and vape then
+        vape:CreateNotification('SobSense', 'Failed to load : '..err, 30, 'alert')
+    end
+    return res
+end
+local queue_on_teleport = queue_on_teleport or queueonteleport or function() end
+local isfile = isfile or function(file)
+    local suc, res = pcall(function()
+        return readfile(file)
+    end)
+    return suc and res ~= nil and res ~= ''
+end
+local cloneref = cloneref or function(obj)
+    return obj
+end
+local playersService = cloneref(game:GetService('Players'))
+
 local function downloadFile(path, func)
     if not isfile(path) then
         local suc, res = pcall(function()
@@ -12,4 +41,73 @@ local function downloadFile(path, func)
         writefile(path, res)
     end
     return (func or readfile)(path)
+end
+
+local function finishLoading()
+    vape.Init = nil
+    vape:Load()
+    task.spawn(function()
+        repeat
+            vape:Save()
+            task.wait(10)
+        until not vape.Loaded
+    end)
+
+    local teleportedServers
+    vape:Clean(playersService.LocalPlayer.OnTeleport:Connect(function()
+        if (not teleportedServers) and (not shared.VapeIndependent) then
+            teleportedServers = true
+            local teleportScript = [[
+                shared.vapereload = true
+                if shared.VapeDeveloper then
+                    loadstring(readfile('newvape/loader.lua'), 'loader')()
+                else
+                    loadstring(game:HttpGet('https://raw.githubusercontent.com/irisdotcclol/SobSenseBD/main/loader.lua', true), 'loader')()
+                end
+            ]]
+            if shared.VapeDeveloper then
+                teleportScript = 'shared.VapeDeveloper = true\n'..teleportScript
+            end
+            vape:Save()
+            queue_on_teleport(teleportScript)
+        end
+    end))
+
+    if not shared.vapereload then
+        if not vape.Categories then return end
+        if vape.Categories.Main.Options['GUI bind indicator'].Enabled then
+            vape:CreateNotification('SobSense Loaded', vape.VapeButton and 'Press the button in the top right to open GUI' or 'Press '..table.concat(vape.Keybind, ' + '):upper()..' to open GUI', 5)
+        end
+    end
+end
+
+if not isfile('newvape/profiles/gui.txt') then
+    writefile('newvape/profiles/gui.txt', 'new')
+end
+local gui = readfile('newvape/profiles/gui.txt')
+
+if not isfolder('newvape/assets/'..gui) then
+    makefolder('newvape/assets/'..gui)
+end
+vape = loadstring(downloadFile('newvape/guis/'..gui..'.lua'), 'gui')()
+shared.vape = vape
+
+if not shared.VapeIndependent then
+    loadstring(downloadFile('newvape/games/universal.lua'), 'universal')()
+    task.spawn(function(...)
+        if isfile('newvape/games/'..game.PlaceId..'.lua') then
+            loadstring(readfile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
+        else
+            local suc, res = pcall(function()
+                return game:HttpGet('https://raw.githubusercontent.com/irisdotcclol/SobSenseBD/main/games/'..game.PlaceId..'.lua', true)
+            end)
+            if suc and res ~= '404: Not Found' then
+                loadstring(downloadFile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
+            end
+        end
+    end)
+    finishLoading()
+else
+    vape.Init = finishLoading
+    return vape
 end
